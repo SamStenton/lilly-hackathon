ce9a699c029087e3bfe9b9b4896b9d39d0825671
Samuel Stenton <sthstenton@gmail.com>
2016-11-13 11:00:58 +0000

    add database migrations to app

--- app.js
+++ app.js
@@ -5,15 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
+var pgp = require('pg-promise')()
+var pg = require('pg');
+var env = require('dotenv').config()
+if (process.env.ENV!=="local") {pg.defaults.ssl = true;}
 
+
+
+// require('./config/migrations.js')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//Migrations
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(64) not null, email text, age integer, cookieId text);')
+  .on('error', function(error) {
+    console.log(error)
+  });
+  client.query('CREATE TABLE IF NOT EXISTS tiles(id SERIAL PRIMARY KEY, user_id text not null, data text, create_at timestamp);')
+  .on('error', function(error) {
+    console.log(error)
+  });
+});
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -39,15 +58,6 @@ fs.readdirSync('./Controllers').forEach(function (file) {
   }
 });
 
-// app.use('/', index);
-// app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 // error handler
 app.use(function(err, req, res, next) {
