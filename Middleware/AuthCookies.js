a74a14f8c6eb41fbf439e851336cede5b24485be
Samuel Stenton <sthstenton@gmail.com>
2016-11-13 17:55:33 +0000

    Issues with middleware

--- Middleware/AuthCookies.js
+++ Middleware/AuthCookies.js
@@ -4,16 +4,18 @@ module.exports.middleware = function(app) {
 	//Cookie Middleware
 	app.use(function (req, res, next) {
 		var cookies = req.cookies
-
-		if (cookies.userCookie === undefined && req.url !== '/user/create') {
-			res.redirect('/user/create');
-		}
-
+		
 		User.findUserById(cookies.userCookie, function(err, user) {
 			if (err) {console.log(err)}
 			req.user = JSON.parse(user);
 			next()
 		});
+
+		if (cookies.userCookie === undefined && req.url !== '/user/create') {
+			res.redirect('/user/create');
+		}
+
+		// next()	
 	})
 
 }
