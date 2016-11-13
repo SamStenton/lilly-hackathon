7bd41b5a58ccd0b510021ab8084e272a6961c949
Samuel Stenton <sthstenton@gmail.com>
2016-11-13 13:06:34 +0000

    User controller changes

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,15 +1,24 @@
 module.exports.controller = function(app) {
+	var shortid = require('shortid')
 	var User = require('../Models/User')
-	/**
-	 * a home page route
-	 */
+
+	app.get('/user/create', function(req, res) {
+		if (req.cookies.userCookie !== undefined) {
+			res.redirect('/');
+		}
+
+		res.cookie('userCookie', shortid.generate(), { maxAge: (10000 * 365 * 24 * 60 * 60), httpOnly: true })
+		res.render('user/create');
+	});
+	
 	app.post('/user/create', function(req, res) {
-		var newUser = new User(req.body)
-		res.cookie('userCookie', newUser.id, { maxAge: (10000 * 365 * 24 * 60 * 60), httpOnly: true })
+		var newUser = new User(req.cookies.userCookie, req.body)
+		// res.cookie('userCookie', newUser.id, { maxAge: (10000 * 365 * 24 * 60 * 60), httpOnly: true })
 		res.send(newUser.id)
 	});
-	app.get('/user/all', function(req, res) {
-		console.log(User.all())
+
+	app.get('/user/account', function(req, res) {
+		res.render('user/account', {user: req.user});
 	});
 	app.get('/user/reset', function(req, res) {
 		res.clearCookie('userCookie');
